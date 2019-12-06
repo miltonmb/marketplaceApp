5a6209bba40628b0a7c730969a52760fd9847246
miltonmb <miltonmb@unitec.edu>
2019-12-06 01:15:23 -0600

    agregar funciona

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
+import Web3 from 'web3'
 import logo from '../logo.png';
 import './App.css';
-import Web3 from 'web3'
-import Navbar from './Navbar'
 import Marketplace from '../abis/Marketplace.json'
+import Navbar from './Navbar'
+import Main from './Main'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      productCount: 0,
-      products: [],
-      loading: true
-    }
-    this.createProduct = this.createProduct.bind(this)
-  }
 
   async componentWillMount() {
     await this.loadWeb3()
     await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
   }
 
   async loadBlockchainData() {
@@ -33,11 +37,31 @@ class App extends Component {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
       const productCount = await marketplace.methods.productCount().call()
-      console.log(productCount.toString())
+      this.setState({ productCount })
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        console.log(productCount.toString())
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
       this.setState({ loading: false })
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
     }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true
+    }
+
+    this.createProduct = this.createProduct.bind(this)
   }
 
   createProduct(name, price) {
@@ -48,34 +72,19 @@ class App extends Component {
       })
   }
 
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-ethereum browser detected.')
-    }
-  }
   render() {
     return (
       <div>
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="/"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" height='200px' />
-                </a>
-                <h1>Marketplace</h1>
-              </div>
+            <main role="main" className="col-lg-12 d-flex">
+              {this.state.loading
+                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                : <Main
+                  products={this.state.products}
+                  createProduct={this.createProduct} />
+              }
             </main>
           </div>
         </div>
