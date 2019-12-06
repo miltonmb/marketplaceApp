65f68826a3d60c32f286249eb8913e0027b6362b
miltonmb <miltonmb@unitec.edu>
2019-12-05 23:51:05 -0600

    buy and sell product complete

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,6 +1,9 @@
 const Marketplace = artifacts.require('./Marketplace.sol')
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
 
-contract('Marketplace', (accounts) => {
+contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace
 
     before(async () => {
@@ -20,6 +23,69 @@ contract('Marketplace', (accounts) => {
             const name = await marketplace.name()
             assert.equal(name, 'Marketplace')
         })
+    })
 
+    describe('products', async () => {
+        let result, productCount
+
+        before(async () => {
+            result = await marketplace.createProduct('Samsung Galaxy s5', web3.utils.toWei('1', 'Ether'), { from: seller })
+            productCount = await marketplace.productCount()
+        })
+
+        it('creates products', async () => {
+            // exito
+            assert.equal(productCount, 1)
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'Samsung Galaxy s5', 'name is correct')
+            assert.equal(event.price, '1000000000000000000', 'price is correct')
+            assert.equal(event.owner, seller, 'owner is correct')
+            assert.equal(event.purchased, false, 'purchased is correct')
+
+            // Falla el producto tiene que tener nombre
+            await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
+            // Falla el producto tiene que tener precio
+            await await marketplace.createProduct('Samsung Galaxy s5', 0, { from: seller }).should.be.rejected;
+        })
+        it('sells products', async () => {
+            // ver la cantidad de fondos
+            let oldSellerBalance
+            oldSellerBalance = await web3.eth.getBalance(seller)
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+            // exito se realizo una venta
+            result = await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether') })
+
+            // Check logs
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'Samsung Galaxy s5', 'name is correct')
+            assert.equal(event.price, '1000000000000000000', 'price is correct')
+            assert.equal(event.owner, buyer, 'owner is correct')
+            assert.equal(event.purchased, true, 'purchased is correct')
+
+            // ver si el seller consiguio los fondos
+            let newSellerBalance
+            newSellerBalance = await web3.eth.getBalance(seller)
+            newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+            let price
+            price = web3.utils.toWei('1', 'Ether')
+            price = new web3.utils.BN(price)
+
+            const exepectedBalance = oldSellerBalance.add(price)
+
+            assert.equal(newSellerBalance.toString(), exepectedBalance.toString())
+
+            // Falla al comprar un producto que no existe
+            await marketplace.purchaseProduct(99, { from: buyer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;      // FAILURE: Buyer tries to buy without enough ether
+            // Falla al comprar con fondos innecesarios
+            await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('0.5', 'Ether') }).should.be.rejected;
+            // Falla el usuario intenta comprar el producto, por ejemplo dos veces
+            await marketplace.purchaseProduct(productCount, { from: deployer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+            // Falla el comprador no puede ser el vendedor
+            await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+        })
     })
 })
